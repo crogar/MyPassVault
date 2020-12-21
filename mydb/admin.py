83b664c61eabff6f2f52cf94c07b9ae84f068915
crogar <58400421+crogar@users.noreply.github.com>
2020-12-20 19:35:19 -0500

    Corrected Models
    Corrected typos in models
    and corrected field names.

--- mydb/admin.py
+++ mydb/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from mydb.models import AccessRecord, Account, Topic
 
 # Register your models here.
+
+admin.site.register(AccessRecord)
+admin.site.register(Account)
+admin.site.register(Topic)
+
+
+
--- mydb/models.py
+++ mydb/models.py
@@ -13,13 +13,21 @@ class Topic(models.Model):  # inheriting from models --- Topic is Service field 
 class Account(models.Model):  # this will handle the Website Name and URL
     topic = models.ForeignKey(Topic, on_delete=models.DO_NOTHING)  # Service in database
     Email = models.CharField(max_length=100)  # Email in database
-    Address = models.CharField(max_length=250)  # Address in database
-    PhoneNumber = models.CharField(max_length=25)  # Phone Number in database
     website_name = models.CharField(max_length=100)  # website_name in database
     url = models.URLField(unique=True)  # website in database
     password = models.CharField(max_length=100)
     userName = models.CharField(max_length=100)
     Name = models.CharField(max_length=100)
+    Address = models.CharField(max_length=250)  # Address in database
+    Phone_Number = models.CharField(max_length=25)  # Phone Number in database
 
     def __str__(self):
-        return self.url
+        return self.website_name
+
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
