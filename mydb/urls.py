aa1ec84fc9bb992897e97d474e512925d8e7e573
crogar <58400421+crogar@users.noreply.github.com>
2020-12-23 16:23:38 -0500

    added forms
    Added forms.py
    added forms.html

--- mydb/urls.py
+++ mydb/urls.py
@@ -3,5 +3,6 @@ from mydb import views
 
 urlpatterns = [
     url(r'^$', views.front, name='front'),
-    url('accounts', views.vault, name='vault')
+    url('accounts', views.vault, name='vault'),
+    url('formpage', views.form_name_view, name='form_name')
 ]
--- mydb/views.py
+++ mydb/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http.request import HttpRequest
 from mydb.models import Account
+from mydb import forms
+
 
 # Create your views here.
+
 
 def front(request):
     return render(request, 'mydb/front.html', context=None)
@@ -12,3 +15,9 @@ def vault(request):
     accounts_list = Account.objects.order_by('url')
     passwords = {"accounts": accounts_list}
     return render(request, 'mydb/accounts.html', context=passwords)
+
+
+def form_name_view(request):
+    form = forms.FormName()
+    return render(request, 'mydb/form.html', context={'form': form})
+
