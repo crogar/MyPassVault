33461e90c2cbc3d0b375b844fec5d457b36f2e44
crogar <58400421+crogar@users.noreply.github.com>
2020-12-24 00:41:59 -0500

    forms edited

--- mydb/forms.py
+++ mydb/forms.py
@@ -1,7 +1,19 @@
 from django import forms
+from django.core import validators
 
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    verifyemail = forms.EmailField(label='Enter Your Email Again')
     text = forms.CharField(widget=forms.Textarea)
+    bot_catcher = forms.CharField(required=False, widget=forms.HiddenInput,
+                                  validators=[validators.MaxLengthValidator(0)])
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verifyemail']
+
+        if email != vmail:
+            raise forms.ValidationError("E-mails don't match")
